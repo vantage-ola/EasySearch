404190859c90efe360517ea245a63d3df3bb79ad
vantage-ola <vantageola4@gmail.com>
2021-09-04 07:22:23 +0100

    Fix: Database Uri

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 class Config():
     """Web Server Default Debug Config"""
-    SQLALCHEMY_DATABASE_URI ='mysql+pymysql://ola:TdKY1o).vSr@YToB/192.168.64.2/Easysearch'
+    SQLALCHEMY_DATABASE_URI ='mysql+pymysql://ola:rkEVq9Z9gZXIsRem@192.168.64.2/Easysearch'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "!@@@B&E&&*@icca"
