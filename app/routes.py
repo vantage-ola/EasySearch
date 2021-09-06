5f80b5265edd5d2078a42fcfbe37489d23874e4f
vantage-ola <vantageola4@gmail.com>
2021-09-06 17:58:32 +0100

    Test Flask Marshmellow Api

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,17 @@
 from app import app
+from flask import request,jsonify
+from models import db, Info, info_schema
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['POST'])
 def get_data():
-    return "Hello, World"
+    name = request.json['name']
+    location = request.json['location']
+    url = request.json['url']
+    address = request.json['address']
+    phone_no = request.json['phone_no']
+
+    info = Info(name,location,url,address,phone_no)
+    db.session.add(info)
+    db.session.commit()
+
+    return info_schema.jsonify(info)
