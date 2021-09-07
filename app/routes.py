d7d8ddf679d7ae5cdba5d5cd43561d0538f2c0e2
vantage-ola <vantageola4@gmail.com>
2021-09-07 14:39:24 +0100

    Added routes

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
 from app import app
 from flask import request,jsonify
-from models import db, Info, info_schema
+from models import db, Info, info_schema, infos_schema
 
 @app.route('/', methods=['POST'])
-def get_data():
+def addData():
     name = request.json['name']
     location = request.json['location']
     url = request.json['url']
@@ -15,3 +15,36 @@ def get_data():
     db.session.commit()
 
     return info_schema.jsonify(info)
+
+@app.route('/get', methods = ['GET'])
+def getData():
+    all_info = Info.query.all()
+    results = infos_schema.dump(all_info)
+
+    return jsonify(results)
+
+@app.route('/get/<id>/', methods = ['GET'])
+def getOneData(id):
+    one_info = Info.query.get(id)
+    
+    return info_schema.jsonify(one_info)
+
+@app.route('/update/<id>/', methods= ['PUT'])
+def updateData(id):
+    one_info = Info.query.get(id)
+
+    name = request.json['name']
+    location = request.json['location']
+    url = request.json['url']
+    address = request.json['address']
+    phone_no = request.json['phone_no']
+
+@app.route('/delete/<id>/', methods= ['DELETE'])
+def deleteData(id):
+    one_info = Info.query.get(id)
+
+    db.session.delete(id)
+    db.session.commit()
+
+    return info_schema.jsonify(one_info)
+
