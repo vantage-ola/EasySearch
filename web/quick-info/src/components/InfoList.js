e569ba8b48396161e98fad67996deb5077cfc8a3
vantage-ola <vantageola4@gmail.com>
2021-09-09 18:59:46 +0100

    Styled Info into cards

--- web/quick-info/src/components/InfoList.js
+++ web/quick-info/src/components/InfoList.js
@@ -5,14 +5,21 @@ function InfoList(props) {
         <div>
             {props.Info && props.Info.map(info=> {
         return (
-          <div key = {info.id}> 
-            <h2>{info.name}</h2>
-            <h3>{info.location}</h3>
-            <h3>{info.address}</h3>
-            <h4>{info.phone_no}</h4>
-            <h5>{info.url}</h5>
-          </div> 
-
+          <div className = "info-result" key = {info.id}>
+            <div className = "row">
+              <div className ="col-sm-6">
+              <div className = "card" style={{width: '20rem'}}>
+                <div className = "card-body">
+                  <h5 className = "card-title">{info.name}</h5>
+                  <h6 className = "card-subtitle mb-2 text-muted">{info.location}</h6>
+                  <p className = "card-text">{info.address}</p>
+                  <a  className = "card-link">{info.phone_no}</a>
+                  <a  className = "card-link">{info.url}</a>
+                </div>
+              </div>
+              </div>
+            </div>
+          </div>
         )
       })}
         </div>
