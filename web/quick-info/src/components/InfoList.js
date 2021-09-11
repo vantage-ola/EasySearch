1df33794f7f8b62c1f61776a91eca10687677cc9
vantage-ola <vantageola4@gmail.com>
2021-09-11 10:53:25 +0100

    Changed div classes

--- web/quick-info/src/components/InfoList.js
+++ web/quick-info/src/components/InfoList.js
@@ -5,20 +5,18 @@ function InfoList(props) {
         <div>
             {props.Info && props.Info.map(info=> {
         return (
-          <div className = "info-result" key = {info.id}>
-            <div className = "row">
-              <div className ="col-sm-6">
-              <div className = "card" style={{width: '20rem'}}>
+          <div key = {info.id}>
+            <div className ="card-group">
+              <div className = "card">
                 <div className = "card-body">
                   <h5 className = "card-title">{info.name}</h5>
                   <h6 className = "card-subtitle mb-2 text-muted">{info.location}</h6>
                   <p className = "card-text">{info.address}</p>
-                  <a  className = "card-link">{info.phone_no}</a>
-                  <a  className = "card-link">{info.url}</a>
+                  <a href = "tel:{info.phone_no}" className = "card-link">{info.phone_no}</a>
+                  <a href = {info.url} className = "card-link">{info.url}</a>
                 </div>
               </div>
               </div>
-            </div>
           </div>
         )
       })}
