2d255293ccce47ec23c5af293c0a9b5883805c79
vantage-ola <vantageola4@gmail.com>
2021-09-16 06:02:37 +0100

    Restructure App.js

--- web/quick-info/src/App.js
+++ web/quick-info/src/App.js
@@ -1,29 +1,16 @@
 import './App.css';
-import {useState, useEffect } from 'react';
-import InfoList from './components/InfoList'
+import InfoList from './components/InfoList';
 import Simpluix from './img/SIMPLUIX-1.png';
+import Home from './components/Home';
 
 function App() {
 
-  const [Info, setInfo] = useState([])
-  useEffect(() => {
-    fetch('http://127.0.0.1:5000/get', {
-      'method': 'GET',
-      headers: {
-        'Content-Type':'application/json'
-      }
-      
-    })
-    .then(resp => resp.json())
-    .then(resp => setInfo(resp))
-    .catch(error => console.log(error))
-  },[])
   return (
     <div className="App">
       <header>
           <img className="logo" src={Simpluix}/>
       </header>
-          <InfoList Info = {Info}/>
+     <Home/> 
     </div>
   );
 }
