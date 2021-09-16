cef78d25e1e6f2eec294875e33c735b208d8827a
vantage-ola <vantageola4@gmail.com>
2021-09-16 20:27:45 +0100

    created useInfoFetch hook

---
+++ web/quick-info/src/hooks/useInfoFetch.js
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+
+import API from '../Api'
+
+const initialState = {
+
+}
+
+export const useInfoFetch = () => {
+
+    const [state, setState] = useState([]);
+    const [loading, setLoading] = useState(false);   
+    const [error, setError] = useState(false);
+
+    const fetchInfos = async () => {
+        try {
+            setError(false);
+            setLoading(true);
+
+            const infos = await API.fetchInfos();
+            setState(infos);
+
+
+        } catch (error) {
+            setError(true);
+        }
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchInfos()
+    }, []);
+
+    return { state, loading, error};
+};
