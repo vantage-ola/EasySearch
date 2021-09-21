2a2ded8f55e1acd5341975cde53f929b168311da
vantage-ola <vantageola4@gmail.com>
2021-09-21 19:25:53 +0100

    Cleaned Code

--- web/quick-info/src/components/Home.js
+++ web/quick-info/src/components/Home.js
@@ -2,15 +2,24 @@ import React, { useState, useEffect } from 'react';
 
 import API from '../Api';
 
+// HOOK
 import { useInfoFetch } from '../hooks/useInfoFetch';
+
+// COMPONENTS
 import InfoList from './InfoList';
+import SearchBar from './SearchBar';
+import LoadSpinner from './LoadSpinner';
 
 const Home = () => {
  
     const { state, loading, error } = useInfoFetch();
 
     return(
-        <div><InfoList Info = {state}/></div>
+        <div>
+            <SearchBar/>
+            <InfoList Info = {state}/>
+            <LoadSpinner/>
+        </div>
     
         );
 
