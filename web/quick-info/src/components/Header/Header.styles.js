2b4d64227641ffa8f09daad1f84b866fbb61a7c8
vantage-ola <vantageola4@gmail.com>
2021-09-26 04:54:51 +0100

    Made little changes for mobile

--- web/quick-info/src/components/Header/Header.styles.js
+++ web/quick-info/src/components/Header/Header.styles.js
@@ -2,23 +2,31 @@ import styled from "styled-components";
 
 
 export const Wrapper = styled.div`
-    padding: 0 20px;
-    
+    padding: 0 20px;    
 `;
 
 export const Content = styled.div`
-    display: flex;
-    align_items: center;
-    justify-content: space-between;
-    max-width: 1280px;
-    padding: 20px 0;
-    
 
+    height: 8em;
+    text-align: center;
+    padding: 15px;
 
+    @media (min-width: 730px) {
+        display: flex;
+        align_items: center;
+        justify-content: space-between;
+        max-width: 1280px;
+        padding: 31px 0;
+      }
 `;
 
 export const LogoImg = styled.img`
-    width: 17%;
-    margin: 15px;   
+    width: 35%;
+    margin: 15px ;
 
+    @media (min-width: 730px) {
+        width: 17%;
+        margin: 15px;
+      
+      }   
 `;
