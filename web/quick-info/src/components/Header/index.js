c289dc6e8bf6baf3469ac23177d383ac3de93a34
vantage-ola <vantageola4@gmail.com>
2021-10-04 13:28:38 +0100

    Added Addbutton and tooltip

--- web/quick-info/src/components/Header/index.js
+++ web/quick-info/src/components/Header/index.js
@@ -4,15 +4,20 @@ import React from 'react';
 
 // IMG
 import SimpluixImg  from '../../img/SIMPLUIX-1.png';
+import AddButton  from '../../img/add-icon.png';
 
 // STYLES
 import { Wrapper , Content , LogoImg , NavButton} from './Header.styles';
 
+//COMPONENTS
+import ToolTip from "../ToolTip/ToolTip";
 const Header = () => (
     <Wrapper>
         <Content>
             <LogoImg src = {SimpluixImg} alt ='simpluix-logo'/>
-            <NavButton>Add Info</NavButton>
+            <ToolTip content="Add Info" direction="top"> 
+                <NavButton type="image" src = {AddButton} readOnly></NavButton>
+            </ToolTip>
         </Content>
     </Wrapper>
 );
