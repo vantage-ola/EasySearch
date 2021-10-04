f96d73988cf50bb19d8031933ae9318be312e7a7
vantage-ola <vantageola4@gmail.com>
2021-10-04 13:29:56 +0100

    Added tooltip feature

---
+++ web/quick-info/src/components/ToolTip/ToolTip.js
@@ -0,0 +1,39 @@
+/* https://paladini.dev/posts/how-to-make-an-extremely-reusable-tooltip-component-with-react--and-nothing-else/ */
+
+import React, { useState } from "react";
+
+const ToolTip = (props) => {
+  let timeout;
+  const [active, setActive] = useState(false);
+
+  const showTip = () => {
+    timeout = setTimeout(() => {
+      setActive(true);
+    }, props.delay || 400);
+  };
+
+  const hideTip = () => {
+    clearInterval(timeout);
+    setActive(false);
+  };
+
+  return (
+    <div
+      className="Tooltip-Wrapper"
+      // When to show the tooltip
+      onMouseEnter={showTip}
+      onMouseLeave={hideTip}
+    >
+      {/* Wrapping */}
+      {props.children}
+      {active && (
+        <div className={`Tooltip-Tip ${props.direction || "top"}`}>
+          {/* Content */}
+          {props.content}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ToolTip;
