8a47b44c0b5ebfd34a7c0e83ace42ef87577e220
vantage-ola <vantageola4@gmail.com>
2021-10-04 13:25:39 +0100

    Added tooltip css

--- web/quick-info/src/HomeStyle.js
+++ web/quick-info/src/HomeStyle.js
@@ -4,7 +4,11 @@ export const HomeStyle = createGlobalStyle`
     :root {
         --white: whitesmoke;
         --blue: #2196F3;
-        --blueblack: rgb(15, 18, 22)
+        --blueblack: rgb(15, 18, 22);
+        --tooltip-text-color: white;
+        --tooltip-background-color: black;
+        --tooltip-margin: 30px;
+        --tooltip-arrow-size: 6px;
     }
     * {
         color: var(--white);
@@ -31,5 +35,90 @@ export const HomeStyle = createGlobalStyle`
         background-color: var(--blueblack);
     }
     
-
+    
+    /* Wrapping */
+    .Tooltip-Wrapper {
+      display: inline-block;
+      position: relative;
+    }
+    
+    /* Absolute positioning */
+    .Tooltip-Tip {
+      position: absolute;
+      border-radius: 4px;
+      left: 50%;
+      transform: translateX(-50%);
+      padding: 6px;
+      color: var(--tooltip-text-color);
+      background: var(--tooltip-background-color);
+      font-size: 14px;
+      font-family: sans-serif;
+      line-height: 1;
+      z-index: 100;
+      white-space: nowrap;
+    }
+    
+    /* CSS border triangles */
+    .Tooltip-Tip::before {
+      content: " ";
+      left: 50%;
+      border: solid transparent;
+      height: 0;
+      width: 0;
+      position: absolute;
+      pointer-events: none;
+      border-width: var(--tooltip-arrow-size);
+      margin-left: calc(var(--tooltip-arrow-size) * -1);
+    }
+    
+    /* Absolute positioning */
+    .Tooltip-Tip.top {
+      top: calc(var(--tooltip-margin) * -1);
+    }
+    /* CSS border triangles */
+    .Tooltip-Tip.top::before {
+      top: 100%;
+      border-top-color: var(--tooltip-background-color);
+    }
+    
+    /* Absolute positioning */
+    .Tooltip-Tip.right {
+      left: calc(100% + var(--tooltip-margin));
+      top: 50%;
+      transform: translateX(0) translateY(-50%);
+    }
+    /* CSS border triangles */
+    .Tooltip-Tip.right::before {
+      left: calc(var(--tooltip-arrow-size) * -1);
+      top: 50%;
+      transform: translateX(0) translateY(-50%);
+      border-right-color: var(--tooltip-background-color);
+    }
+    
+    /* Absolute positioning */
+    .Tooltip-Tip.bottom {
+      bottom: calc(var(--tooltip-margin) * -1);
+    }
+    /* CSS border triangles */
+    .Tooltip-Tip.bottom::before {
+      bottom: 100%;
+      border-bottom-color: var(--tooltip-background-color);
+    }
+    
+    /* Absolute positioning */
+    .Tooltip-Tip.left {
+      left: auto;
+      right: calc(100% + var(--tooltip-margin));
+      top: 50%;
+      transform: translateX(0) translateY(-50%);
+    }
+    /* CSS border triangles */
+    .Tooltip-Tip.left::before {
+      left: auto;
+      right: calc(var(--tooltip-arrow-size) * -2);
+      top: 50%;
+      transform: translateX(0) translateY(-50%);
+      border-left-color: var(--tooltip-background-color);
+    }
+    
 `;
